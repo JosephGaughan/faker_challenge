da0052e83077f300ad7caaa9f2c489e10e6f758a
Paul Gilson <paul.gilson@makers.tech>
2023-11-17 16:17:11 +0000

    add unit8 resources

---
+++ unit8/starter/app/src/test/java/starter/DefectTest.java
@@ -0,0 +1,27 @@
+package starter;
+
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+// complete the test and dataprovider method
+// the goal is to practice parameterised testing
+// you don't need to use a spreadsheet (yet)
+
+public class DefectTest {
+    @Test(dataProvider = "")
+    public void isValidReturnsFalseForInvalidDefects(String name, String summary, String details) {
+        // comment these out once your data provider method works
+        System.out.println("Test case");
+        System.out.println(" - Name: " + name);
+        System.out.println(" - Summary: " + summary);
+        System.out.println(" - Details: " + details);
+        // add your test code below
+    }
+
+    @DataProvider(name = "")
+    public Object[][] invalidDefectData() {
+        // return a 2D array of inputs and expected urls
+        // you can hard code it for now
+        return null;
+    }
+}
